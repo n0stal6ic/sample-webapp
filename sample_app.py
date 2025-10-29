35fdd1a8d8f0867a0ae23242f565012bff7b2d23
n0stal6ic <owen2856@gmail.com>
2025-10-29 00:05:49 -0500

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template("index.html", request=request)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
